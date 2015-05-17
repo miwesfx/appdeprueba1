3da8427342aa3e5fb74a50c37686a96a0143bcd6
miwesfx <Miguel@MacBook-Air-de-Miguel.local>
2015-05-17 21:08:45 +0200

    Creación app vacía
    Creando la app sin hacer nada aun

---
+++ Primera APP/Primera APP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Primera APP
+//
+//  Created by Miguel on 17/5/15.
+//  Copyright (c) 2015 Miguel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Primera APP/Primera APP/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Primera APP
+//
+//  Created by Miguel on 17/5/15.
+//  Copyright (c) 2015 Miguel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
